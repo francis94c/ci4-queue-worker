b1d45ff9ef445a616f5df81f06aad55d5af4e153
Francis Ilechukwu <francis94c@gmail.com>
2021-11-30 20:50:04 +0100

    REDIS Worker.

--- src/Drivers/Redis/Worker.php
+++ src/Drivers/Redis/Worker.php
@@ -2,6 +2,8 @@
 
 namespace Francis94c\CI4QueueWorker\Drivers\Redis;
 
+use CodeIgniter\CLI\CLI;
+use Exception;
 use Francis94c\CI4QueueWorker\Interfaces\WorkerDriver;
 
 class Worker implements WorkerDriver
@@ -9,16 +11,23 @@ class Worker implements WorkerDriver
     public function work(\Config\Queue $config)
     {
         $redis = new \Redis();
+
         $redis->connect('127.0.0.1', 6379);
 
         while (true) {
             // Fetch a job from the queue.
-            $jobData = $redis->blPop($config->queue ?? 'queue');
-            $jobData = json_decode($jobData[1]);
+            $jobData = $redis->blPop($config->queue ?? 'queue', 2);
+            $job = unserialize($jobData[1] ?? null);
 
-            if (!$jobData) continue;
+            if ($job) {
+                try {
+                    $job->handle();
+                } catch (Exception $e) {
+                    CLI::error($e->getMessage());
+                    $redis->rPush($config->failedQueue ?? 'failed_jobs', $jobData[1]);
+                }
+            }
 
-            $job = new $jobData->job(...$jobData->payload);
             sleep($config->sleep ?? 3);
         }
         // $redis->lPush('queue', 'test1');
