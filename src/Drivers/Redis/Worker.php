b1b7614ae7ab8848abb1df08063347b452bd081d
Francis Ilechukwu <francis94c@gmail.com>
2021-11-30 21:57:08 +0100

    Feedbacks.

--- src/Drivers/Redis/Worker.php
+++ src/Drivers/Redis/Worker.php
@@ -17,23 +17,22 @@ class Worker implements WorkerDriver
         while (true) {
             // Fetch a job from the queue.
             $jobData = $redis->blPop($config->queue ?? 'queue', 2);
+
+            // Attempt to Un-Serialize a Job Object.
             $job = unserialize($jobData[1] ?? null);
 
             if ($job) {
+                CLI::write('Executing Job: ' . get_class($job), 'green');
                 try {
                     $job->handle();
                 } catch (Exception $e) {
-                    CLI::error($e->getMessage());
+                    CLI::write('An Error Occurred: ' . $e->getMessage(), 'red');
                     $redis->rPush($config->failedQueue ?? 'failed_jobs', $jobData[1]);
+                    CLI::write('Job Failed and Moved to Failed Queue', 'red');
                 }
             }
 
             sleep($config->sleep ?? 3);
         }
-        // $redis->lPush('queue', 'test1');
-        // $redis->lPush('queue', 'test2');
-        // print($redis->iLen('queue'));
-        // print_r($redis->blPop('queue', 10));
-        // print_r($redis->blPop('queue', 10));
     }
 }
